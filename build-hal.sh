674720cf1c9cc4eb85a3f6ff99edad19825623f4
Flytreels <66539732+Flytreels@users.noreply.github.com>
2021-07-08 22:21:24 +0800

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
-source halium.env
+. halium.env
 cd $ANDROID_ROOT
-
-
-source build/envsetup.sh
+. build/envsetup.sh
 #bash hybris-patches/apply-patches.sh --mb
 export USE_CCACHE=1
 breakfast $DEVICE
-make -j128 mkbootimg
-make -j128 fec
-make -j128 halium-boot
-
-#bash hybris-patches/apply-patches.sh --mb
-#touch /home/runner/work/halium/out/soong/.intermediates/frameworks/base/packages/EasterEgg/EasterEgg/android_common/dex/EasterEgg.jar
-make -j128 systemimage 
-
-echo "md5sum halium-boot.img and system.img"
-#md5sum $ANDROID_ROOT/out/target/product/kiwi/halium-boot.img
-#md5sum $ANDROID_ROOT/out/target/product/kiwi/system.img
+#mka -j128 mkbootimg
+#mka -j128 fec
+#mka -j128 halium-boot
+mka -j128 systemimage
