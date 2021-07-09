5852f97ec4f8faba0b2e05645a531ee0b30074b8
Flytreels <66539732+Flytreels@users.noreply.github.com>
2021-07-09 09:08:42 +0800

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -6,6 +6,6 @@ bash hybris-patches/apply-patches.sh --mb
 export USE_CCACHE=1
 breakfast $DEVICE
 mka -j 4 mkbootimg
-mka -j 4 fec
+#mka -j 4 fec
 mka -j 4 halium-boot
-mka -j 4 systemimage
+#mka -j 4 systemimage
